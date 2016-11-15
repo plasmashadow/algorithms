2aa4976f134aa8fb44fe08ddb2afdff8ea1d48c2
plasmashadow <plasmashadowx@gmail.com>
2016-11-15 08:09:50 +0530

    Removed non abstracted graph interfaces

--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -3,12 +3,10 @@ import unittest
 
 from algorithms.data_structures import (
     binary_search_tree,
-    digraph,
     queue,
     linked_list,
     stack,
     graph,
-    undirected_graph,
     union_find,
     union_find_by_rank,
     union_find_with_path_compression,
@@ -402,106 +400,6 @@ class TestGraphInterface(unittest.TestCase):
         self.assertEqual([1, 3, 5], self.gp.find_path(1, 5))
 
 
-class TestDirectedGraph(unittest.TestCase):
-
-    """
-    Test Undirected Graph Implementation
-    """
-
-    def test_directed_graph(self):
-
-        # init
-        self.dg0 = digraph.Digraph()
-        self.dg1 = digraph.Digraph()
-        self.dg2 = digraph.Digraph()
-        self.dg3 = digraph.Digraph()
-
-        # populating
-        self.dg1.add_edge(1, 2)
-
-        self.dg1_rev = self.dg1.reverse()  # reverse
-
-        self.dg2.add_edge(1, 2)
-        self.dg2.add_edge(1, 2)
-
-        self.dg3.add_edge(1, 2)
-        self.dg3.add_edge(1, 2)
-        self.dg3.add_edge(3, 1)
-
-        # test adj
-        self.assertTrue(2 in self.dg1.adj(1))
-        self.assertEqual(len(self.dg1.adj(1)), 1)
-        self.assertTrue(1 not in self.dg1.adj(2))
-        self.assertEqual(len(self.dg1.adj(2)), 0)
-
-        self.assertTrue(1 in self.dg1_rev.adj(2))
-        self.assertEqual(len(self.dg1_rev.adj(2)), 1)
-        self.assertTrue(2 not in self.dg1_rev.adj(1))
-        self.assertEqual(len(self.dg1_rev.adj(1)), 0)
-
-        self.assertTrue(2 in self.dg2.adj(1))
-        self.assertEqual(len(self.dg2.adj(1)), 2)
-        self.assertTrue(1 not in self.dg2.adj(2))
-        self.assertEqual(len(self.dg2.adj(2)), 0)
-
-        self.assertTrue(2 in self.dg3.adj(1))
-        self.assertTrue(1 in self.dg3.adj(3))
-        self.assertEqual(len(self.dg3.adj(1)), 2)
-        self.assertTrue(1 not in self.dg3.adj(2))
-        self.assertEqual(len(self.dg3.adj(2)), 0)
-        self.assertTrue(3 not in self.dg3.adj(1))
-        self.assertEqual(len(self.dg3.adj(3)), 1)
-
-        # test degree
-        self.assertEqual(self.dg1.outdegree(1), 1)
-        self.assertEqual(self.dg1.outdegree(2), 0)
-
-        self.assertEqual(self.dg1_rev.outdegree(2), 1)
-        self.assertEqual(self.dg1_rev.outdegree(1), 0)
-
-        self.assertEqual(self.dg2.outdegree(1), 2)
-        self.assertEqual(self.dg2.outdegree(2), 0)
-
-        self.assertEqual(self.dg3.outdegree(1), 2)
-        self.assertEqual(self.dg3.outdegree(2), 0)
-        self.assertEqual(self.dg3.outdegree(3), 1)
-
-        # test vertices
-        self.assertEqual(list(self.dg0.vertices()), [])
-        self.assertEqual(len(self.dg0.vertices()), 0)
-
-        self.assertTrue(1 in self.dg1.vertices())
-        self.assertTrue(2 in self.dg1.vertices())
-        self.assertEqual(len(self.dg1.vertices()), 2)
-
-        self.assertTrue(2 in self.dg1_rev.vertices())
-        self.assertTrue(1 in self.dg1_rev.vertices())
-        self.assertEqual(len(self.dg1_rev.vertices()), 2)
-
-        self.assertTrue(1 in self.dg2.vertices())
-        self.assertTrue(2 in self.dg2.vertices())
-        self.assertEqual(len(self.dg2.vertices()), 2)
-
-        self.assertTrue(1 in self.dg3.vertices())
-        self.assertTrue(2 in self.dg3.vertices())
-        self.assertTrue(3 in self.dg3.vertices())
-        self.assertEqual(len(self.dg3.vertices()), 3)
-
-        # test vertex_count
-        self.assertEqual(self.dg0.vertex_count(), 0)
-        self.assertEqual(self.dg1.vertex_count(), 2)
-        self.assertEqual(self.dg1_rev.vertex_count(), 2)
-        self.assertEqual(self.dg2.vertex_count(), 2)
-        self.assertEqual(self.dg3.vertex_count(), 3)
-
-        # test edge_count
-        self.assertEqual(self.dg0.edge_count(), 0)
-        self.assertEqual(self.dg1.edge_count(), 1)
-        self.assertEqual(self.dg1_rev.edge_count(), 1)
-        self.assertEqual(self.dg2.edge_count(), 2)
-        self.assertEqual(self.dg3.edge_count(), 3)
-
-
 class TestQueue(unittest.TestCase):
 
     """
@@ -576,89 +474,6 @@ class TestStack(unittest.TestCase):
         self.assertEqual(self.sta.remove(), 2)
         self.assertEqual(self.sta.is_empty(), False)
         self.assertEqual(self.sta.size(), 3)
-
-
-class TestUndirectedGraph(unittest.TestCase):
-
-    """
-    Test Undirected Graph Implementation
-    """
-
-    def test_undirected_graph(self):
-
-        # init
-        self.ug0 = undirected_graph.Undirected_Graph()
-        self.ug1 = undirected_graph.Undirected_Graph()
-        self.ug2 = undirected_graph.Undirected_Graph()
-        self.ug3 = undirected_graph.Undirected_Graph()
-
-        # populating
-        self.ug1.add_edge(1, 2)
-
-        self.ug2.add_edge(1, 2)
-        self.ug2.add_edge(1, 2)
-
-        self.ug3.add_edge(1, 2)
-        self.ug3.add_edge(1, 2)
-        self.ug3.add_edge(3, 1)
-
-        # test adj
-        self.assertTrue(2 in self.ug1.adj(1))
-        self.assertEqual(len(self.ug1.adj(1)), 1)
-        self.assertTrue(1 in self.ug1.adj(2))
-        self.assertEqual(len(self.ug1.adj(1)), 1)
-
-        self.assertTrue(2 in self.ug2.adj(1))
-        self.assertEqual(len(self.ug2.adj(1)), 2)
-        self.assertTrue(1 in self.ug2.adj(2))
-        self.assertEqual(len(self.ug2.adj(1)), 2)
-
-        self.assertTrue(2 in self.ug3.adj(1))
-        self.assertTrue(3 in self.ug3.adj(1))
-        self.assertEqual(len(self.ug3.adj(1)), 3)
-        self.assertTrue(1 in self.ug3.adj(2))
-        self.assertEqual(len(self.ug3.adj(2)), 2)
-        self.assertTrue(1 in self.ug3.adj(3))
-        self.assertEqual(len(self.ug3.adj(3)), 1)
-
-        # test degree
-        self.assertEqual(self.ug1.degree(1), 1)
-        self.assertEqual(self.ug1.degree(2), 1)
-        self.assertEqual(self.ug2.degree(1), 2)
-        self.assertEqual(self.ug2.degree(2), 2)
-        self.assertEqual(self.ug3.degree(1), 3)
-        self.assertEqual(self.ug3.degree(2), 2)
-        self.assertEqual(self.ug3.degree(3), 1)
-
-        # test vertices
-        self.assertEqual(list(self.ug0.vertices()), [])
-        self.assertEqual(len(self.ug0.vertices()), 0)
-
-        self.assertTrue(1 in self.ug1.vertices())
-        self.assertTrue(2 in self.ug1.vertices())
-        self.assertEqual(len(self.ug1.vertices()), 2)
-
-        self.assertTrue(1 in self.ug2.vertices())
-        self.assertTrue(2 in self.ug2.vertices())
-        self.assertEqual(len(self.ug2.vertices()), 2)
-
-        self.assertTrue(1 in self.ug3.vertices())
-        self.assertTrue(2 in self.ug3.vertices())
-        self.assertTrue(3 in self.ug3.vertices())
-        self.assertEqual(len(self.ug3.vertices()), 3)
-
-        # test vertex_count
-        self.assertEqual(self.ug0.vertex_count(), 0)
-        self.assertEqual(self.ug1.vertex_count(), 2)
-        self.assertEqual(self.ug2.vertex_count(), 2)
-        self.assertEqual(self.ug3.vertex_count(), 3)
-
-        # test edge_count
-        self.assertEqual(self.ug0.edge_count(), 0)
-        self.assertEqual(self.ug1.edge_count(), 1)
-        self.assertEqual(self.ug2.edge_count(), 2)
-        self.assertEqual(self.ug3.edge_count(), 3)
-
 
 class TestUnionFind(unittest.TestCase):
 
